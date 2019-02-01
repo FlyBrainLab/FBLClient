cb53fb803f218211f1f2fd7c35627fd03e7b171f
Mehmet Kerem Turkcan <mkt2126@columbia.edu>
2019-02-01 15:43:57 -0500

    Adding FBL examples.

---
+++ examples/python_scripts/3_networkx.py
@@ -0,0 +1,18 @@
+# For using both FFBO and FBL efficiently and correctly, you should learn 
+# to use NetworkX, a graph library for Python.
+# You can access the documentation here:
+# https://networkx.github.io/documentation/stable/
+
+# Let us start with a basic query and get the connectivity dendrogram:
+nm[0].executeNLPquery('show GABAergic neurons in the ellipsoid body')
+nm[0].getConnectivityDendrogram()
+# We can now access the circuit graph as follows:
+circuit_graph = nm[0].C.G
+# We can for example look at a list of nodes:
+print(circuit_graph.nodes(data=True))
+# Or the synapses:
+print(circuit_graph.edges(data=True))
+# This circuit is automatically generated in a Neurokernel-executable format.
+
+# For simulations, it is recommended that you start to look at Neurodriver:
+# https://github.com/neurokernel/neurodriver
---
+++ examples/python_scripts/6_executing_circuits.py
@@ -0,0 +1,15 @@
+# In this demo, we show how one can perform circuit execution.
+# We have an example script for the execution of a cartridge:
+# load_retina_lamina
+
+# We can use it as follows:
+experiment_configuration = nm[0].load_retina_lamina(cartridgeIndex=126)
+experiment_configuration = experiment_configuration['success']['result']
+nm[0].execute_multilpu(experiment_configuration)
+# You will be notified in the front end when the execution results are obtained.
+# Simulation data is kept in a stack:
+my_sim_data = nm[0].data[-1]
+# We can plot our results as follows:
+B, keys = nm[0].getSimResults()
+nm[0].plotSimResults(B, keys)
+# Note that these scripts only provide a bare-bones implementation.
