5cefc0cdfa4060c7c3beac20438e50003e4bf1c7
Yiyin Zhou <yiyin@ee.columbia.edu>
2021-02-09 16:53:23 -0500

    bump version to 1.1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ install_requires = [
 
 setup(
     name="FlyBrainLab",
-    version="1.0",
+    version="1.1",
     description="Main Client of the FlyBrainLab Project",
     author="Mehmet Kerem Turkcan",
     author_email="mkt2126@columbia.edu",
