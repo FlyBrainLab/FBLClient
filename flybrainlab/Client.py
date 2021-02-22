e7aff6c876edef2608baaea79eb072f73295c781
Yiyin Zhou <yiyin@ee.columbia.edu>
2021-02-22 10:46:04 -0500

    check for github for latest version instead

--- flybrainlab/Client.py
+++ flybrainlab/Client.py
@@ -131,17 +131,25 @@ def check_version(min_version_supported_by_NeuroMynerva):
     check_FBLClient_version(min_version_supported_by_NeuroMynerva)
     return True
 
-def check_for_update():
+def check_for_update_pip():
     name = 'FBLClient'
     latest_version = str(subprocess.run([sys.executable, '-m', 'pip', 'install', '--use-deprecated=legacy-resolver', '{}=='.format(name)], capture_output=True, text=True))
     latest_version = latest_version[latest_version.find('(from versions:')+15:]
     latest_version = latest_version[:latest_version.find(')')]
     latest_version = latest_version.replace(' ','').split(',')[-1]
 
-    current_version = str(subprocess.run([sys.executable, '-m', 'pip', 'show', '{}'.format(name)], capture_output=True, text=True))
-    current_version = current_version[current_version.find('Version:')+8:]
-    current_version = current_version[:current_version.find('\\n')].replace(' ','') 
+    current_version = fbl.__version__ 
 
+    if latest_version == current_version:
+        return 'FBLClient is up to date.'
+    else:
+        return 'Update {} is available for FBLClient.'.format(latest_version)
+
+def check_for_update():
+    response = requests.get("https://api.github.com/repos/flybrainlab/FBLClient/releases/latest")
+    latest_version = response.json()["name"]
+
+    current_version = fbl.__version__
     if latest_version == current_version:
         return 'FBLClient is up to date.'
     else:
