40bfb1dd2811971dc60ce26ab9cb7bc2e9e86627
Yiyin Zhou <yiyin@ee.columbia.edu>
2021-02-23 02:28:33 -0500

    update version to 1.1.0

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ install_requires = [
 
 setup(
     name="FlyBrainLab",
-    version="1.1",
+    version="1.1.0",
     description="Main Client of the FlyBrainLab Project",
     author="Mehmet Kerem Turkcan",
     author_email="mkt2126@columbia.edu",
