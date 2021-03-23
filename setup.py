5175f5a800f44ac40a490ff9255d7f31c2af8ccd
Yiyin Zhou <yiyin@ee.columbia.edu>
2021-03-23 03:23:19 -0400

    update setup.py to include extras_require for utilities

--- setup.py
+++ setup.py
@@ -12,21 +12,34 @@ install_requires = [
     "seaborn",
     "requests",
     "dataclasses; python_version<'3.7'",
-    'msgpack',
-    'msgpack-numpy',
-    'neuroballad @ git+https://github.com/flybrainlab/neuroballad',
-    'service_identity',
-    'crochet',
-    'matplotlib',
-    'fastcluster',
-    'networkx',
-    'pandas',
-    'scipy',
-    'sympy',
-    'nose',
-    'jupyterlab>=2.2.8',
-    'pywin32; platform_system=="Windows"'
+    "msgpack",
+    "msgpack-numpy",
+    "neuroballad @ git+https://github.com/flybrainlab/neuroballad",
+    "service_identity",
+    "crochet",
+    "matplotlib",
+    "fastcluster",
+    "networkx",
+    "pandas",
+    "scipy",
+    "sympy",
+    "nose",
+    "jupyterlab>=2.2.8, <=3.0.10",
+    "pywin32; platform_system=='Windows'"
 ]
+
+extras_require_utilities = {
+        "gem @ git+https://github.com/palash1992/GEM.git",
+        "graspy",
+        "umap-learn",
+        "tensorflow",
+        "sklearn",
+        "cdlib",
+        "pyclustering",
+        "nxcontrol @ git+https://github.com/mkturkcan/nxcontrol"
+    }
+
+extras_require_full = extras_require_utilities
 
 setup(
     name="FlyBrainLab",
@@ -38,7 +51,13 @@ setup(
     download_url="",
     license="BSD-3-Clause",
     install_requires=install_requires,
-    dependency_links=['https://github.com/flybrainlab/neuroballad/tarball/master#egg=neuroballad-0.1.0',
-                      'https://github.com/mkturkcan/autobahn-sync/tarball/master#egg=autobahn_sync-0.3.2'],
+    extras_require = {
+        "full": extras_require_full,
+        "utilities": extras_require_utilities,
+    }
+    dependency_links=["https://github.com/flybrainlab/neuroballad/tarball/master#egg=neuroballad-0.1.0",
+                      "https://github.com/mkturkcan/autobahn-sync/tarball/master#egg=autobahn_sync-0.3.2",
+                      "https://github.com/palash1992/GEM/tarball/master#egg=gem-1.0.0",
+                      "https://github.com/mkturkcan/nxcontrol/tarball/master#egg=nxcontrol-0.2"],
     packages=find_packages(),
 )
