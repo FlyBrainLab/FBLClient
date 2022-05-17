a2a3ae8330b75e3407c069df795ebfd6869ff698
Yiyin Zhou <yiyin@ee.columbia.edu>
2022-05-17 11:27:28 -0400

    use GEM from a more update-to-date fork, remove dependency_links

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ install_requires = [
 ]
 
 extras_require_utilities = {
-        "gem @ git+https://github.com/palash1992/GEM.git",
+        "nxt_gem @ git+https://github.com/jernsting/nxt_gem",
         "graspy<=0.1.1",
         "scipy",
         "umap-learn",
@@ -60,9 +60,5 @@ setup(
         "full": extras_require_full,
         "utilities": extras_require_utilities,
     },
-    dependency_links=["https://github.com/flybrainlab/neuroballad/tarball/master#egg=neuroballad-0.1.0",
-                      "https://github.com/mkturkcan/autobahn-sync/tarball/master#egg=autobahn_sync-0.3.2",
-                      "https://github.com/palash1992/GEM/tarball/master#egg=gem-1.0.0",
-                      "https://github.com/mkturkcan/nxcontrol/tarball/master#egg=nxcontrol-0.2"],
     packages=find_packages(),
 )
