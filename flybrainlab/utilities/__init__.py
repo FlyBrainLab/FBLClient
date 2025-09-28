fb589c58d25bded85a7cdd2754b97570b18d35aa
Yiyin Zhou <yiyin.zhou@fordham.edu>
2025-09-28 14:09:48 -0400

    move neurowatch into main src dir

--- flybrainlab/utilities/__init__.py
+++ flybrainlab/utilities/__init__.py
@@ -3,7 +3,6 @@ These utilities can be accessed by importing flybrainlab.utilities.
 """
 
 from .neurometry import *
-from .neurowatch import *
 from .neurograph import *
 from .neuroembed import *
 from .neurosynapsis import *
